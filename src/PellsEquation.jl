31aec5e0771b15f362ac9e31ec02520b440c9149
byhill <byhill.xi1ej@dralias.com>
2024-08-19 07:10:26 -0600

    Update  docstring

--- src/PellsEquation.jl
+++ src/PellsEquation.jl
@@ -100,22 +100,19 @@ end
 """
     continued_fraction(D::Integer, P::Integer=0, Q::Integer=1)
 
-Returns an iterator that gives a continued fraction to the degree-two algebraic number
-
-    x = (sqrt(D) + P) / Q.
+Gives the continued fraction of the quadractic number `x = (sqrt(D) + P) / Q`.
 
 More specifically,
-the iterator `continued_fraction(D, P, Q)` returns
+the iterator `continued_fraction(D, P, Q)` returns 3-Tuples of the form
 
     (ai::BigInt, Pi::BigInt, Qi::BigInt),
 
-where ai is the i'th coefficent of the continued fraction,
-and Pi / Qi is the i'th convergent.
+where `ai` is the `i`'th coefficent of the continued fraction,
+and `Pi / Qi` is the `i`'th convergent of `x`.
 
-```julia-repl
-julia> using PellsEquation, .Iterators
-
-julia> collect(take(continued_fraction(14, -9, -13), 12))
+# Examples
+```jldoctest
+julia> collect(Iterators.take(continued_fraction(14, -9, -13), 12))
 12-element Vector{Tuple{Int64, BigInt, BigInt}}:
  (0, 0, 1)
  (2, 1, 2)
